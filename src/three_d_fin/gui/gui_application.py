00fb8223068e983872f81f6c5b055775c06ddddc
romain janvier <romain.janvier@hotmail.fr>
2023-05-25 17:11:50 +0200

    Minor formatting fixes

--- src/three_d_fin/gui/gui_application.py
+++ src/three_d_fin/gui/gui_application.py
@@ -1,18 +1,20 @@
-import sys
 import os
+import sys
 from pathlib import Path
 
-from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog, QFileDialog
 from PyQt5 import QtCore, QtGui
+from PyQt5.QtWidgets import QApplication, QDialog, QFileDialog, QMainWindow
 
-from three_d_fin.gui.main_window import Ui_MainWindow
 from three_d_fin.gui.expert_dlg import Ui_Dialog
+from three_d_fin.gui.main_window import Ui_MainWindow
+
 
 class ExpertDialog(QDialog):
     def __init__(self, parent=None):
         super().__init__(parent)
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
+
 
 class MainWindow(QMainWindow):
     def __init__(self, parent=None):
@@ -26,13 +28,13 @@ class MainWindow(QMainWindow):
 
         # Click on the "documentation"
         self.ui.documentation_link_btn.clicked.connect(self.show_documentation)
-        
+
         # Click on the "?" button on the expert table
         self.ui.expert_info_btn.clicked.connect(self.show_expert_dialog)
 
         # Click on input
         self.ui.input_file_btn.clicked.connect(self.input_file_clicked)
-    
+
         # Click on output
         self.ui.output_dir_btn.clicked.connect(self.output_dir_clicked)
 
@@ -40,7 +42,7 @@ class MainWindow(QMainWindow):
         self.ui.compute_btn.clicked.connect(self.compute_clicked)
 
     def show_expert_dialog(self):
-        """Show the expert help/warning dialog"""
+        """Show the expert help/warning dialog."""
         dialog = ExpertDialog(self)
         dialog.show()
 
@@ -53,13 +55,15 @@ class MainWindow(QMainWindow):
             base_path = Path(sys._MEIPASS)
         except Exception:
             base_path = Path(__file__).absolute().parents[1] / "documentation"
-        QtGui.QDesktopServices.openUrl(QtCore.QUrl.fromLocalFile(str(Path(base_path / "documentation.pdf"))));
+        QtGui.QDesktopServices.openUrl(
+            QtCore.QUrl.fromLocalFile(str(Path(base_path / "documentation.pdf")))
+        )
 
     def input_file_clicked(self):
         input_dialog = QFileDialog(self)
         if input_dialog.exec_():
             self.ui.input_file_in.setText(input_dialog.selectedFiles()[0])
-    
+
     def output_dir_clicked(self):
         input_dialog = QFileDialog(self)
         if input_dialog.exec_():
@@ -68,6 +72,7 @@ class MainWindow(QMainWindow):
     def compute_clicked(self):
         placeholder = QDialog(self)
         placeholder.show()
+
 
 if __name__ == "__main__":
     os.environ["QT_AUTO_SCREEN_SCALE_FACTOR"] = "1"
