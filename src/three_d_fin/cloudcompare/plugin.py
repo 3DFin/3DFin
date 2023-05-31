8e062ceddaeff429901b7de3ec6491d641dc4edc
romain janvier <romain.janvier@hotmail.fr>
2023-05-31 10:28:09 +0200

    Restore original CC Plugin behavior.
    the culprit is not 100% on our code. it seems that the async nature of QT's show method does not cope well with current CC-PythonPlugin architecture see https://github.com/tmontaigu/CloudCompare-PythonPlugin/issues/39#issuecomment-1569657823 for description

--- src/three_d_fin/cloudcompare/plugin.py
+++ src/three_d_fin/cloudcompare/plugin.py
@@ -1,10 +1,10 @@
 from pathlib import Path
-from Q
 
 import pycc
 
 from three_d_fin.cloudcompare.plugin_processing import CloudComparePluginProcessing
 from three_d_fin.gui.application import Application
+
 
 class ThreeDFinCC(pycc.PythonPluginInterface):
     """Define a CloudCompare-PythonPlugin Plugin (sic.)."""
@@ -36,6 +36,30 @@ class ThreeDFinCC(pycc.PythonPluginInterface):
             pycc.Action(name="3D Forest INventory", icon=self.getIcon(), target=main)
         ]
 
+
+def _create_app_and_run(
+    plugin_processing: CloudComparePluginProcessing, scalar_fields: list[str]
+):
+    """Encapsulate the 3DFin GUI and processing.
+
+    It also embed a custom fix for the HiDPI support that is broken when using tk
+    under the CloudCompare runtime. This function allow to run the fix and the app
+    on a dedicated thread thanx to pycc.RunInThread.
+
+    Parameters
+    ----------
+    plugin_processing : CloudComparePluginProcessing
+        The instance of FinProcessing dedicated to CloudCompare (as a plugin)
+    scalar_fields : list[str]
+        A list of scalar field names. These list will feed the dropdown menu
+        inside the 3DFin GUI.
+    """
+    plugin_widget = Application(
+        plugin_processing, file_externally_defined=True, cloud_fields=scalar_fields
+    )
+    plugin_widget.show()
+
+
 def main():
     """3DFin CloudCompare Plugin main action."""
     cc = pycc.GetInstance()
@@ -56,10 +80,16 @@ def main():
         scalar_fields.append(point_cloud.getScalarFieldName(i))
 
     # TODO: Handle big coodinates (could be tested but maybe wait for CC API update).
-    plugin_processing = CloudComparePluginProcessing(cc, point_cloud)
+    plugin_functor = CloudComparePluginProcessing(cc, point_cloud)
 
-    #cc.freezeUI(True)
-    plugin_widget = Application(plugin_processing, True, scalar_fields)
-    plugin_widget.show()
-        #cc.freezeUI(False)
-    #cc.updateUI()
+    cc.freezeUI(True)
+    try:
+        pycc.RunInThread(_create_app_and_run, plugin_functor, scalar_fields)
+        # _create_app_and_run(plugin_functor, scalar_fields)
+    except Exception:
+        raise RuntimeError(
+            "Something went wrong!"
+        ) from None  # TODO: Catch exceptions into modals.
+    finally:
+        cc.freezeUI(False)
+        cc.updateUI()
